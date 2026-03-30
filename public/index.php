a678c575fc2a87e7598533e93b39d132d5b578be
Ayoub <a.reggad@ecole-ipssi.net>
2026-03-30 18:13:27 +0200

    feat(Ayoub): initialisation du Front Controller avec session et erreurs

---
+++ public/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * REVIEWEO - Front Conrtoller
+ * Auteur : Ayoub
+ */
+
+// 1. demarrage de la session (T17)
+session_start();
+
+//2 affichage des erreurs (Utile pour le dev)
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL); // On demande d'afficher TOUS les types d'erreurs
+
