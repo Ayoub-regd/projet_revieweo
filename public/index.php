b8397463d073a24d9eeba599e14796e5dbeb3bb5
Ayoub <a.reggad@ecole-ipssi.net>
2026-03-30 20:00:43 +0200

    feat(Ayoub): finalisation de l'autoloader dans l'index

--- public/index.php
+++ public/index.php
@@ -1,15 +1,33 @@
 <?php
 
 /**
- * REVIEWEO - Front Conrtoller
+ * REVIEWEO - Front Controller
  * Auteur : Ayoub
  */
 
-// 1. demarrage de la session (T17)
+// 1. Démarrage de la session
 session_start();
 
-//2 affichage des erreurs (Utile pour le dev)
+// 2. Affichage des erreurs (utile pour le développement)
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); // On demande d'afficher TOUS les types d'erreurs
+error_reporting(E_ALL);
 
+// 3. L'autoloader
+spl_autoload_register(function ($class) {
+    $prefix = 'App\\';
+    $base_dir = __DIR__ . '/../app/';
+
+    // On vérifie si la classe utilise notre préfixe
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+
+    if (file_exists($file)) {
+        require_once $file;
+    }
+});
